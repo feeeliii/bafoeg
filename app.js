a11683dfba403824dc52cbf445898824e0211c9e
feeeliii <felicitas.strickmann@code.berlin>
2024-09-27 21:43:43 +0200

    first routing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 const PORT = 3000
@@ -8,5 +8,28 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (request, response) => {
-    response.send('Welcome to my Bafögrant!')
-  })
+    response.send(`
+        <form action="/submit-story" method="post">
+            <textarea name="story"></textarea>
+            <button type="submit">Geschichte einreichen</button>
+        </form>
+    `);
+});
+
+app.post('/submit-story', (request, response) => {
+    response.send('Vielen Dank für das Einreichen der Geschichte. Nach einem kurzen Checkup wird deine Geschichte online gehen.')
+})
+
+app.get('/about', (request, response) => {
+    response.send('Über das Projekt')
+})
+
+app.get('/greet', (request, response) => {
+    const name = 'Besucher';
+    const timeOfDay = 'Abend';
+    response.send(`<p>${name}, Willkommen auf unserer Webseite.</p>`);
+});
+
+
+
+
