66a27ffc772392d48ef066a0a501674a1e6cba8b
feeeliii <felicitas.strickmann@code.berlin>
2024-09-30 19:03:06 +0200

    routing

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.get('/', (request, response) => {
             <textarea name="story"></textarea>
             <button type="submit">Geschichte einreichen</button>
         </form>
-    `);
-});
+    `)
+})
 
 app.post('/submit-story', (request, response) => {
     response.send('Vielen Dank für das Einreichen der Geschichte. Nach einem kurzen Checkup wird deine Geschichte online gehen.')
@@ -27,38 +27,36 @@ app.get('/about', (request, response) => {
 app.get('/greet', (request, response) => {
     const name = 'Besucher';
     const timeOfDay = 'Abend';
-    response.send(`<p>Liebe*r ${name}, Willkommen auf unserer Webseite.</p>`);
-});
+    response.send(`<p>Liebe*r ${name}, Willkommen auf unserer Webseite.</p>`)
+})
 
 
 
 // Routes for Bafög experiences / Bafög Erfahrungen
 
 app.get('/experiences', (request, response) => {
-    response.send('List of all Bafög experiences');
-});
+    response.send('List of all Bafög experiences')
+})
 
 app.post('/experiences', (request, response) => {
-    response.send(`New experience added: ${request.body.text}`);
-});
+    response.send(`New experience added: ${request.body.text}`)
+})
 
 app.get('/experiences/:slug', (request, response) => {
     const experienceSlug = request.params.slug;
-    response.send(`Details of experience with slug: ${experienceSlug}`);
-});
+    response.send(`Details of experience with slug: ${experienceSlug}`)
+})
 
 // Routes for Bafög demands / Forderungen
 app.get('/demands', (request, response) => {
-    response.send('List of all Bafög demands');
-});
+    response.send('List of all Bafög demands')
+})
 
 app.post('/demands', (request, response) => {
-    response.send(`New demand added: ${request.body.text}`);
-});
+    response.send(`New demand added: ${request.body.text}`)
+})
 
 app.get('/demands/:slug', (request, response) => {
     const demandSlug = request.params.slug;
-    response.send(`Details of demand with slug: ${demandSlug}`);
-});
-
-
+    response.send(`Details of demand with slug: ${demandSlug}`)
+})
