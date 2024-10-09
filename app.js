2a6d31c050ec89e1ee5e78f99ccc0f49f5731b02
feeeliii <felicitas.strickmann@code.berlin>
2024-10-09 13:39:45 +0200

    routes

--- app.js
+++ app.js
@@ -9,23 +9,45 @@ app.listen(PORT, () => {
 
 //new
 
+//object for experiences and demands
+
+const experiences = [
+    {id: 1, content: 'bla', author: 'Heiz Heinzel', date: '01.01.2024'},
+    {id: 2, content: 'bla', author: 'Berta Bertanius', date: '02.02.2024'}
+]
+
+const demands = [
+    {id: 1, content: 'bla', author: 'Heiz Heinzel', date: '01.01.2024'},
+    {id: 2, content: 'bla', author: 'Berta Bertanius', date: '02.02.2024'}
+]
+
 app.get('/', (request, response) => {
     response.send('Liste aller Bafög Geschichten');
-});
+})
 
 app.get('/stories/:id', (request, response) => {
     const storyId = parseInt(request.params.id, 10);
-    response.send(`Details zu Geschichte ${storyId}`);
-});
+    const story = experiences.find(exp => exp.id === storyId);
+    if (story) {
+        response.json(story);
+    } else {
+        response.status(404).send('Geschichte nicht gefunden, sorry :(')
+    }
+})
 
 app.get('/demands', (request, response) => {
-  response.send('Liste aller Forderungen:');
-});
+    response.send('Liste aller Forderungen:');
+  });
 
 app.get('/demands/:id', (request, response) => {
-  const demandId = parseInt(request.params.id, 10);
-  response.send(`Details zur Forderung mit ID: ${demandId}`);
-});
+    const demandId = parseInt(request.params.id, 10);
+    const demand = demands.find(dem => dem.id === demandId);
+    if (demand) {
+      response.json(demand);
+    } else {
+      response.status(404).send('Forderung nicht gefunden, sorry :(');
+    }
+  });
 
 app.get('/project', (request, response) => {
     response.send('Über das Projekt');
@@ -34,3 +56,4 @@ app.get('/project', (request, response) => {
 app.get('/privacy', (request, response) => {
     response.send('Datenschutz');
 });
+
