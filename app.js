c0c44b5716b2dbbe90381f29ecbbca209ecd1902
feeeliii <felicitas.strickmann@code.berlin>
2024-11-04 20:18:09 +0100

    set view engine

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ mongoose.connect(dbURI)
 
 // Middleware to parse URL-encoded data (form data)
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 app.set('views', path.join(path.resolve(), 'views'));
 app.use(logger);
 app.use('/assets', express.static('public'));
